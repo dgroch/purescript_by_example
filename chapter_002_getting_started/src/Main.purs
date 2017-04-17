2cffc242b010cd9231ee85ee3e04f923abfd703b
Dan Groch <daniel.j.groch@gmail.com>
2017-04-17 16:49:50 +1000

    circleArea function

--- chapter_002_getting_started/src/Main.purs
+++ chapter_002_getting_started/src/Main.purs
@@ -3,10 +3,15 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Math (sqrt)
+import Math (pi, sqrt)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
+circleArea :: Number -> Number
+circleArea r = pi * (r * r)
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = logShow (diagonal 3.0 4.0)
+main = do
+  logShow (diagonal 3.0 4.0)
+  logShow (circleArea 5.0)
